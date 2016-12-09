9eec9a1b64461e22fb58f93663688a389bc4c159
Luca Guidi <me@lucaguidi.com>
2016-12-09 00:36:20 +0000

    Initial mess

---
+++ config/environment.rb
@@ -0,0 +1,40 @@
+require 'bundler/setup'
+require 'hanami/setup'
+require 'hanami/model'
+require_relative '../lib/hello_domains'
+require_relative '../apps/web/application'
+
+Hanami.configure do
+  mount Web::Application, at: '/'
+
+  model do
+    ##
+    # Database adapter
+    #
+    # Available options:
+    #
+    #  * SQL adapter
+    #    adapter :sql, 'sqlite://db/hello_domains_development.sqlite3'
+    #    adapter :sql, 'postgres://localhost/hello_domains_development'
+    #    adapter :sql, 'mysql://localhost/hello_domains_development'
+    #
+    adapter :sql, ENV['DATABASE_URL']
+
+    ##
+    # Migrations
+    #
+    migrations 'db/migrations'
+    schema     'db/schema.sql'
+  end
+
+  mailer do
+    root 'lib/hello_domains/mailers'
+
+    # See http://hanamirb.org/guides/mailers/delivery
+    delivery do
+      development :test
+      test        :test
+      # production :smtp, address: ENV['SMTP_PORT'], port: 1025
+    end
+  end
+end
