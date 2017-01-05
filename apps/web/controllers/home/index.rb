2c2cb060fb6871f9b0d22e4c4153cd7fa007c012
Luca Guidi <me@lucaguidi.com>
2017-01-05 19:16:08 +0100

    Extract DNSimple base URL into env var

--- apps/web/controllers/home/index.rb
+++ apps/web/controllers/home/index.rb
@@ -6,7 +6,7 @@ module Web::Controllers::Home
     def call(params)
       return unless signed_in?
 
-      client = Dnsimple::Client.new(base_url: "https://api.sandbox.dnsimple.com", access_token: current_user.token)
+      client = Dnsimple::Client.new(base_url: ENV['DNSIMPLE_BASE_URL'], access_token: current_user.token)
       @domains = client.domains.list_domains(current_user.uid).data
     end
   end
